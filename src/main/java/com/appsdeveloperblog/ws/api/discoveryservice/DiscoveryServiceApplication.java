b54993ba2139584b57b4315eb379b75cbcf98b21
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-08-17 08:27:02 -0400

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/ws/api/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.ws.api.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
